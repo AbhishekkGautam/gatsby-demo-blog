419c34fe0d2dd2c536d0612505804c0c72ad6d2d
Abhishek Gautam <abhishekgautm243@gmail.com>
2020-08-05 01:46:14 +0530

    Update blog-post.js

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,12 +2,21 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { Link } from "gatsby"
+import { Helmet } from "react-helmet"
 
 const Template = ({ data }) => {
   const post = data.markdownRemark
 
   return (
     <Layout>
+      <Helmet>
+        <script
+          async
+          src="https://demo.fewcents.co/static/js/paywall.js"
+          id="fewcents_paywall"
+          contentSelector="blogpost"
+        />
+      </Helmet>
       <SEO title="BlogPost" />
       <Link to="/blog">Go Back</Link>
       <hr />
@@ -15,7 +24,7 @@ const Template = ({ data }) => {
       <h6 style={{ color: "grey" }}>
         Posted by {post.frontmatter.author} on {post.frontmatter.date}
       </h6>
-      <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      <div class="blogpost" dangerouslySetInnerHTML={{ __html: post.html }} />
     </Layout>
   )
 }
